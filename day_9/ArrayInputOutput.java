84ea4f0fb0409612f171714e31fa2ae75a197a31
Varsha M <varsha17806@gmail.com>
2026-09-19 10:28:55 +0530

    Another java program

---
+++ day_9/ArrayInputOutput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ArrayInputOutput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int arr[] = new int[size];
+        for(int i =0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        for(int i =0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
